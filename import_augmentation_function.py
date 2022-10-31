7883003adee58ff26c002c011f33014e57c0312a
Dafni Roumba <dafni.roumbae@gmail.com>
2022-10-31 17:35:48 +0100

    fixes

--- import_augmentation_function.py
+++ import_augmentation_function.py
@@ -43,7 +43,7 @@ def aug_fun(data_set_test_trainvalid_ratio, imarray, imarray_gauss, img_size):
     augmentation_data, validation_data, augmentation_data_gauss, validation_data_gauss =  train_test_split(imarray, imarray_gauss, 
                                                                                                        test_size=data_set_test_trainvalid_ratio, random_state=data_split_state)
     transform = Compose([RandomRotate90(p=0.5), HorizontalFlip(p=0.5), Flip(p=0.5)])
-    aug_data, aug_data_gauss= augStack(augmentation_data, augmentation_data_gauss,transform, sigma=8)
+    aug_data, aug_data_gauss= augStack(augmentation_data, augmentation_data_gauss, transform, sigma=8)
     return aug_data, aug_data_gauss, validation_data, validation_data_gauss
 
 
@@ -56,8 +56,6 @@ def import_aug_fun(joinpath, fdir, imdir):
     all_image_array=np.zeros((256,256))[None, :, :]
     all_data_gauss_aug=np.zeros((256,256))[None, :, :]
     all_data_aug=np.zeros((256,256))[None, :, :]
-
-    data_split_state = None   # Random split on each call
     
 
     for image_file in os.listdir(joinpath):
