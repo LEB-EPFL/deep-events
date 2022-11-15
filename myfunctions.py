e7dc3aacae696170bc66a07298b86f0b7f968ed0
Dafni Roumba <dafni.roumbae@gmail.com>
2022-11-15 13:55:16 +0100

    some changes made

--- myfunctions.py
+++ myfunctions.py
@@ -193,25 +193,18 @@ def image_crop_negative(l,list_of_divisions, data, img, outputname,foldname):
             savepath=os.path.join(foldname,currname_crop_a)
             tifffile.imwrite(savepath, (dataar_a).astype(np.uint16), photometric='minisblack')
 
-
-
-## AUGMENTATION OF DATA (I need to go through this again) ##
-
 def augImg(input_img, output_img, transform, **kwargs):
     #input_mask = (input_img>0).astype(np.uint8)
     transformed = transform(image=input_img, image0=output_img)
     
     aug_input_img, aug_output_img= transformed['image'], transformed['image0']
-    
-    # aug_fission_coords = preprocessing.fissionCoords(aug_labels, aug_output_img)
-    # aug_output_img, aug_fission_props = preprocessing.prepareProc(aug_output_img, coords=aug_fission_coords, **kwargs)
     return aug_input_img.astype(np.float64), aug_output_img
     
 
 def augStack(input_data, output_data, transform, **kwargs):
     aug_input_data = np.zeros(input_data.shape, dtype=np.float64)
     aug_output_data = np.zeros(output_data.shape, dtype=np.float32)
-
+    
     for i in tqdm(range(input_data.shape[0]), total=input_data.shape[0]):
         aug_input_data[i], aug_output_data[i]= augImg(input_data[i], output_data[i], transform, **kwargs)
     return aug_input_data, aug_output_data
