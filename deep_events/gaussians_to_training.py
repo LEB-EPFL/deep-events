c637f5cf882745b17a023fbe5062661f1d921e8f
Emily Niemand <emily.niemand@epfl.ch>
2023-04-11 15:22:12 +0200

    int to floats and tiff, tif

--- deep_events/gaussians_to_training.py
+++ deep_events/gaussians_to_training.py
@@ -9,14 +9,17 @@ from bson.objectid import ObjectId
 import numpy as np
 from multiprocessing import Pool
 
-folder = "Z:/_Lab members/Juan/230222_MitoSplitNet_TrainingSet_U2OS_iSIM/"
-db_files = list(Path(folder).rglob(r'db.yaml'))
+folder = "Z:/_Lab members/Emily/20230329_FtsW_sfGFP_caulobacter_zeiss"
+db_files = list(Path(folder).rglob(r'ground_truth*'))
 SAVING_SCHEME = "ws_0.2"
 
 
 def extract_events(db_file):
     tif_file = sorted(Path(os.path.dirname(db_file)).glob(r'*.ome.tif'), key=os.path.getmtime)[-1]
-    gaussians_file = os.path.join(os.path.dirname(db_file), 'ground_truth.tiff')
+    if os.path.exists(os.path.join(os.path.dirname(db_file), 'ground_truth.tiff')):
+        gaussians_file = os.path.join(os.path.dirname(db_file), 'ground_truth.tiff')
+    else:
+        gaussians_file = os.path.join(os.path.dirname(db_file), 'ground_truth.tif')
 
     folder_dict = get_dict(Path(os.path.dirname(db_file)))
     event_dict = copy.deepcopy(folder_dict)
@@ -26,16 +29,21 @@ def extract_events(db_file):
     event_dict['event_content'] = 'division'
 
     with tifffile.TiffFile(tif_file) as images, tifffile.TiffFile(gaussians_file) as gaussians:
-        events = basic_scan(gaussians.asarray())
+        events = basic_scan(gaussians.asarray(), threshold=0.5)
+        print(events)
         for event in events:
             gaussians_crop, box = crop_images(event, gaussians)
             imgs_crop, box = crop_images(event, images)
             event_dict = handle_db(event, box, event_dict)
 
+            # tifffile.imwrite(os.path.join(event_dict['event_path'], "ground_truth.tif"),
+            #                  (gaussians_crop).astype(np.uint16), photometric='minisblack')
             tifffile.imwrite(os.path.join(event_dict['event_path'], "ground_truth.tif"),
-                             (gaussians_crop).astype(np.uint16), photometric='minisblack')
+                             (gaussians_crop).astype(np.float16), photometric='minisblack')
+            # tifffile.imwrite(os.path.join(event_dict['event_path'], "images.tif"),
+            #                  (imgs_crop).astype(np.uint16), photometric='minisblack')
             tifffile.imwrite(os.path.join(event_dict['event_path'], "images.tif"),
-                             (imgs_crop).astype(np.uint16), photometric='minisblack')
+                             (imgs_crop).astype(np.float16), photometric='minisblack')
 
 
 def handle_db(event, box, event_dict):
@@ -54,6 +62,6 @@ def handle_db(event, box, event_dict):
 
 
 if __name__ == "__main__":
-    shutil.rmtree(os.path.join(folder, "training_data"))
-    with Pool(12) as p:
+    shutil.rmtree(os.path.join(folder, "training_data"), ignore_errors=True)
+    with Pool(5) as p:
         p.map(extract_events, db_files)
