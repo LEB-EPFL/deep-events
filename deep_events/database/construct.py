f8977fbdc2097471d2b81513d71274e8c7d5ca57
Willi L Stepp <willi.stepp@epfl.ch>
2023-04-15 09:59:07 +0200

    Added different database for models

--- deep_events/database/construct.py
+++ deep_events/database/construct.py
@@ -1,36 +1,54 @@
 #%%
-from extract_yaml import MAIN_PATH
+from deep_events.database.extract_yaml import MAIN_PATH
 from pathlib import Path
-import os
 from pymongo import MongoClient
 from benedict import benedict
 
-
-# Initialize database connection
-cluster = "mongodb://lebpc13.epfl.ch/"
-client = MongoClient(cluster)
-coll = client.deep_events['collection']
+def main():
+    construct_from_folder(MAIN_PATH, "mito_events")
 
 
-#%% Get the db.yaml files from the folders
-training_path = os.path.join(MAIN_PATH, "event_data")
-print(training_path)
-event_list = Path(training_path).rglob("*/event_db.yaml")
-event_list = list(event_list)
+def construct_from_folder(folder: Path, collection: str):
+    # Initialize database connection
+    cluster = "mongodb://lebpc13.epfl.ch/"
+    client = MongoClient(cluster)
+    coll = client.deep_events[collection]
 
-print(event_list)
-#%% Reset the database and add all of the events
-coll.delete_many({})
-for event in event_list:
-    event_dict = benedict(str(event))
-    coll.insert_one(event_dict)
+    #%% Get the db.yaml files from the folders
+    if "event_data" in folder:
+        event_list = list(Path(folder).rglob("*/event_db.yaml"))
+        event_list = [benedict(str(event)) for event in event_list]
+    if "training_data" in folder:
+        event_dirs = list(Path(folder).rglob("*/*settings.yaml"))
+        event_list = []
+        for event in event_dirs:
+            db_prompt = event.parents[0] / "db_prompt.yaml"
+            model = str(event).replace("settings", "model")
+            time = str(event.parts[-1]).replace("_settings.yaml","")
+            event = benedict(str(event))
+            for key, value in benedict(str(db_prompt)).items():
+                event[key] = value
+            event["model"] = model
+            event["time"] = time
+            event_list.append(event)
+            print(event)
 
-#%%
-filtered_list = list(coll.find({'microscope': ['isim'], 'cell_line': 'cos7'}))
+
+    #%% Reset the database and add all of the events
+    coll.delete_many({})
+    for event in event_list:
+        coll.insert_one(event)
 
 
-db_files = []
-for item in filtered_list:
-    db_files.append(Path(item['event_path']) / "event_db.yaml")
 
-print("\n".join(db_files))
+def retrieve_filtered_list(coll, prompt = {}):
+    cluster = "mongodb://lebpc13.epfl.ch/"
+    client = MongoClient(cluster)
+    coll = client.deep_events[coll]
+
+    # Example for filtering a collection in the database and retrieving the data as a list
+    filtered_list = list(coll.find({'microscope': 'isim', 'cell_line': 'cos7'}))
+    my_list = [str(item) for item in filtered_list]
+
+    print("\n".join(my_list))
+    return my_list
